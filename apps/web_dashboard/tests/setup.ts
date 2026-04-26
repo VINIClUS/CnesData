61a84c40b82a1eb8fe03367a3ecf8f74619c07f7
Vinícius Andre <vinicius0zero0@gmail.com>
2026-04-26 15:31:47 -0300

    feat(web_dashboard/signup): add AccessRequestForm + PendingRequestsList

--- apps/web_dashboard/tests/setup.ts
+++ apps/web_dashboard/tests/setup.ts
@@ -6,3 +6,18 @@ import { server } from "./mocks/server";
 beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
+
+if (typeof Element !== "undefined") {
+  if (!Element.prototype.hasPointerCapture) {
+    Element.prototype.hasPointerCapture = () => false;
+  }
+  if (!Element.prototype.setPointerCapture) {
+    Element.prototype.setPointerCapture = () => undefined;
+  }
+  if (!Element.prototype.releasePointerCapture) {
+    Element.prototype.releasePointerCapture = () => undefined;
+  }
+  if (!Element.prototype.scrollIntoView) {
+    Element.prototype.scrollIntoView = () => undefined;
+  }
+}
